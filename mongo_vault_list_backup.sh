a0738cadb31e0afd4c2eac6c0c853ad71b7b92fa
lele <lele@datamini.ai>
2024-02-14 00:07:00 +0800

    bugfix

--- mongo_vault_list_backup.sh
+++ mongo_vault_list_backup.sh
@@ -8,7 +8,7 @@ if [ -z "$MONGO_VAULT_OSS_AK" ] || [ -z "$MONGO_VAULT_OSS_SK" ] || [ -z "$MONGO_
 fi
 
 # 配置 OSS 工具
-ossutil config -e $MONGO2OSS_OSS_ENDPOINT -i $MONGO2OSS_OSS_AK -k $MONGO2OSS_OSS_SK
+ossutil config -e $MONGO_VAULT_OSS_ENDPOINT -i $MONGO_VAULT_OSS_AK -k $MONGO_VAULT_OSS_SK
 
 # 计算当前月份和上一个月份
 CURRENT_MONTH=$(date +%Y-%m)
@@ -17,7 +17,7 @@ LAST_MONTH=$(date +%Y-%m --date="1 month ago")
 echo "Listing backups for $CURRENT_MONTH and $LAST_MONTH..."
 
 # 列出当前月份和上一个月份的备份文件
-ossutil ls oss://$MONGO2OSS_OSS_BUCKET/$MONGO2OSS_OSS_URI_PREFIX/$CURRENT_MONTH/ --recursive
-ossutil ls oss://$MONGO2OSS_OSS_BUCKET/$MONGO2OSS_OSS_URI_PREFIX/$LAST_MONTH/ --recursive
+ossutil ls oss://$MONGO_VAULT_OSS_BUCKET/$MONGO_VAULT_OSS_URI_PREFIX/$CURRENT_MONTH/ --recursive
+ossutil ls oss://$MONGO_VAULT_OSS_BUCKET/$MONGO_VAULT_OSS_URI_PREFIX/$LAST_MONTH/ --recursive
 
 echo "Backup listing completed."
