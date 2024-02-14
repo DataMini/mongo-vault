343eaf5e7f9fdcafef8cd26f9dd9bc0b041db5aa
lele <lele@datamini.ai>
2024-02-14 11:36:31 +0800

    bugfix on restore

--- mongo_vault_backup.sh
+++ mongo_vault_backup.sh
@@ -17,6 +17,11 @@ CURRENT_TIME=$(date +%Y%m%d%H%M%S)
 BACKUP_PATH="$MONGO_VAULT_OSS_URI_PREFIX/$CURRENT_MONTH/$CURRENT_TIME"
 echo "Backup path is $BACKUP_PATH."
 
+# 创建临时备份目录
+TEMP_BACKUP_DIR=/tmp/mongo_vault_backup/$CURRENT_TIME
+mkdir -p $TEMP_BACKUP_DIR
+
+
 # 定义要跳过的数据库名称数组
 SKIP_DATABASES=("admin" "config" "local")
 
@@ -33,8 +38,8 @@ should_skip() {
 
 # 获取所有数据库列表或使用指定的数据库
 if [ -z "$MONGO_VAULT_BACKUP_DATABASES" ]; then
-  echo "No databases specified, backing up all databases."
   DATABASES=$(mongo --quiet -u $MONGO_INITDB_ROOT_USERNAME -p $MONGO_INITDB_ROOT_PASSWORD --authenticationDatabase admin --eval "db.getMongo().getDBNames().join(' ');" | tail -n1)
+  echo "No databases specified, backing up all databases. Found databases: $DATABASES"
 else
   echo "Backing up specified databases: $MONGO_VAULT_BACKUP_DATABASES"
   DATABASES=$(echo $MONGO_VAULT_BACKUP_DATABASES | tr ',' ' ')
@@ -48,11 +53,27 @@ for DB in $DATABASES; do
   fi
 
   echo "Backing up database $DB..."
-  mongodump -u $MONGO_INITDB_ROOT_USERNAME -p $MONGO_INITDB_ROOT_PASSWORD --authenticationDatabase admin --db $DB --archive --gzip | ossutil cp - oss://$MONGO_VAULT_OSS_BUCKET/$BACKUP_PATH/${DB}.gz
+  mongodump -u $MONGO_INITDB_ROOT_USERNAME -p $MONGO_INITDB_ROOT_PASSWORD --authenticationDatabase admin --db $DB --archive=$TEMP_BACKUP_DIR/${DB}.gz --gzip
+
+  # mongodump -u $MONGO_INITDB_ROOT_USERNAME -p $MONGO_INITDB_ROOT_PASSWORD --authenticationDatabase admin --db $DB --archive --gzip | ossutil cp - oss://$MONGO_VAULT_OSS_BUCKET/$BACKUP_PATH/${DB}.gz
+  # 检查备份操作是否成功
   if [ $? -eq 0 ]; then
-    echo "Database $DB backup completed."
+    echo "MongoDB backup for database $DB completed successfully."
+
+    # 使用 ossutil 将备份文件上传到 OSS
+    ossutil cp $TEMP_BACKUP_DIR/${DB}.gz oss://$MONGO_VAULT_OSS_BUCKET/$BACKUP_PATH/${DB}.gz
+
+    # 检查上传操作是否成功
+    if [ $? -eq 0 ]; then
+      echo "Backup file uploaded to OSS successfully."
+    else
+      echo "Failed to upload backup file to OSS."
+    fi
+
+    # 删除临时备份文件
+    rm $TEMP_BACKUP_DIR/${DB}.gz
   else
-    echo "Failed to backup database $DB."
+    echo "Failed to backup MongoDB database $DB ."
   fi
 done
 
